cd02f53bda290607d51a498afe1edc734be937b1
Alanys <alanysgre18@gmail.com>
2025-09-21 19:56:21 -0400

    Just updated the program info; no code yet. 09/21/2025

---
+++ AlanysSuazo_ProgrammingExercise_2.py
@@ -0,0 +1,7 @@
+# Alanys Suazo-Gracia
+# Assignment: Programming Exercise 2
+
+# The purpose of this assignment is to create a program that scans user input as a string
+#   after which that sting will be scanned for key phrases seen and used in scam emails. It should return the
+#   number of substrings and set a counter which will tell the user the likeliness of an email being a scam.
+

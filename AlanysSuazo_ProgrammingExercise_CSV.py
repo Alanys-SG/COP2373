c868938886dcbf471fbfa29899d019fe67fa969e
Alanys <alanysgre18@gmail.com>
2025-10-26 17:21:02 -0400

    Initial Upload. Created 10/26/2025. No code written as of yet, just comments.

---
+++ AlanysSuazo_ProgrammingExercise_CSV.py
@@ -0,0 +1,8 @@
+# Alanys Suazo
+# Assignment: Programming Exercise CSV
+
+# The purpose of this assignment is to practice using the CSV package and collecting data to import and export
+#   from this single program. The program should be able to create a csv file and append it with student
+#   first name, last name, and their 3 test scores. After the file has been created and appended, the program
+#   should be able to read the csv file and display the information in table format.
+
